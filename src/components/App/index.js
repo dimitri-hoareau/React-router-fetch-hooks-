71286029035abc4fe92caf4d76a6c7729e5041ca
tonytiratayoclock <tony@oclock.io>
2020-07-20 15:10:04 +0200

    Add loading state

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 // == Import
 import Header from "../Header";
@@ -8,6 +8,7 @@ import Footer from "../Footer";
 import NotFound from "../NotFound";
 import categoriesData from "src/data/categories";
 import postsData from "src/data/posts";
+import Loading from "../Loading";
 import NotFoud from "../NotFound";
 
 /*
@@ -34,24 +35,35 @@ const getFilteredPosts = (category) => {
 
 // == Composant
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="blog">
       <Header categories={categoriesData} />
-      <Switch>
-        {/* { Si loading -> Loading } */}
+      <button
+        onClick={() => {
+          setLoading(!loading);
+        }}
+      >
+        Load
+      </button>
+      {loading && <Loading />}
+      {!loading && (
+        <Switch>
+          {categoriesData.map((category) => {
+            return (
+              <Route key={category.label} exact path={category.route}>
+                <Posts posts={getFilteredPosts(category.label)} />
+              </Route>
+            );
+          })}
+          <Redirect from="/jquery" to="/react" />
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      )}
 
-        {categoriesData.map((category) => {
-          return (
-            <Route key={category.label} exact path={category.route}>
-              <Posts posts={getFilteredPosts(category.label)} />
-            </Route>
-          );
-        })}
-        <Redirect from="/jquery" to="/react" />
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
       <Footer />
     </div>
   );
---
+++ src/components/Loading/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./style.scss";
+const Loading = () => <div className="spinner"></div>;
+
+export default Loading;
